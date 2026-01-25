95b71d1c68d5fb8792f739004370a5c11ff1f60d
Rajasekar811 <111588149+Rajasekar811@users.noreply.github.com>
2026-01-25 08:12:38 +0530

    Add files via upload

--- plugins/user_id.py
+++ plugins/user_id.py
@@ -21,7 +21,7 @@ async def showid(client, message):
 
 
 
-# Tech freak 
+# Lasera_Official
 # Don't Remove Credit!!!
-# Telegram Channel @Tech_freak_tamil
-# Developer @devilo7
+# Telegram Channel @Lasera_Official
+# Developer @raja_sekar_811
